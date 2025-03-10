8257cb67ee4c916e8d760555fadac622878742d1
ChristopherGeidl <christophergeidl@gmail.com>
2025-03-09 20:31:47 -0600

    Add quality settings

--- wave_display.py
+++ wave_display.py
@@ -3,7 +3,10 @@ from OpenGL.GL import *
 import numpy as np
 import math
 
-SIZE = 800
+SIZE = 300
+QUALITY = 7
+LINE_WIDTH = 2*QUALITY + 1
+
 CENTER = SIZE/2
 
 #amplitude -> color value (1 = pure white, -1 = pure black)
@@ -16,21 +19,21 @@ def interpolate_color(amplitude):
     color_value = (amplitude + 1) / 2
     return color_value
 
-def draw_wave(time, x, y):
-    glLineWidth(5)
+def draw_wave(time, x, y, wavelength):
+    glLineWidth(LINE_WIDTH)
     glBegin(GL_LINE_STRIP)
     #normalize pixel x, y to OpenGL -1 to 1
     x = (x - CENTER) / CENTER
     y = (y - CENTER) / CENTER
     
-    
-    for r in np.linspace(-1,1,int(CENTER)):
+    for r in np.linspace(-2,2,int((-1.5*CENTER/5)*QUALITY + (13*CENTER/5))):
         for theta in range(360):
-            color = interpolate_color(math.sin(2*math.pi*(r+time)))
+            color = interpolate_color(math.sin((1/wavelength)*(5/math.pi)*(r-time)))
             glColor3f(color,color,color)
             x_pixel = x + r*math.cos(math.radians(theta))
             y_pixel = y + r*math.sin(math.radians(theta))
-            glVertex2f(x_pixel, y_pixel)
+            if(x_pixel <= 1 or x_pixel >= -1) and (y_pixel <= 1 or y_pixel >= -1):
+                glVertex2f(x_pixel, y_pixel)
     glEnd()
 
 def main():
@@ -50,7 +53,7 @@ def main():
         glLoadIdentity() # resets transformation matrix
         
         time = glfw.get_time()
-        draw_wave(time, CENTER, CENTER)
+        draw_wave(time, CENTER, CENTER, 0.1)
         
         #OpenGL renders to a hidden (back) buffer while the previous frame is still displayed. 
         #This moves the back buffer to the front.
